4bb8bab81e552b9d80144c93c10723c7c3dad0b1
samueltezera28 <samueltezera28@gmail.com>
2023-08-08 21:56:46 +0300

    handleChange

---
+++ src/components/TodoForms.js
@@ -0,0 +1,33 @@
+
+import React, {useState} from 'react'
+
+export default function TodoForm(props) {
+
+
+    const {input, setInput} = useState("");
+
+    const handleChange = e =>{
+        setInput(e.target.value)
+    }
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.addTask({
+            id: Math.floor(Math.random() * 1000000),
+            text:input,
+            isComplete:false
+        })
+        setInput('')
+        
+    }
+
+
+  return (
+  <form className='todo-form'>
+    <input type='text' placeholder='Add a todo' onChange={handleChange} className='todo-input'  value={input} name='text' />
+    <button type='submit' className="todo-btn" onClick={handleSubmit}>Add Todo</button>
+    </form>
+
+);
+  }
+ 
