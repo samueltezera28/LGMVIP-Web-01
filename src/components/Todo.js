ab19d032d830f877ce24f5f3a8f561e9905e426e
samueltezera28 <samueltezera28@gmail.com>
2023-08-08 23:29:26 +0300

    new update

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,18 +1,15 @@
-import React from 'react'
-import{RiCloseCircleLine} from 'react-icons/ri'
-import {TiTick} from 'react-icons/ti'
+import React from 'react';
+import { RiCloseCircleLine } from 'react-icons/ri'
+import { TiTick } from 'react-icons/ti'
 
-export default function Todo({todos, completeTask, removeTask}) {
-
-
-  return todos.map((todo,index)=>(
-    <div className= {todo.iscomplete ? 'complete task-div' : 'task-div'} key={index}>
-        <div className='todo-text'>{todo.text}</div>
-        <div className='icons'>
-            <TiTick className='tick' onclick={() => {completeTask(todo.id)}}></TiTick>
-            <RiCloseCircleLine className='cross' onclick={()=>{removeTask(todo.id)}}></RiCloseCircleLine>
-        </div>
+export default function Todo({ todos, completeTask, removeTask }) {
+  return todos.map((todo, index) => (
+    <div className={todo.isComplete ? 'complete task-div' : 'task-div'} key={index}>
+      <div className="todo-text">{todo.text}</div>
+      <div className="icons" key={todo.id}>
+        <TiTick className={todo.isComplete ? 'hide' : 'tick'} onClick={() => { completeTask(todo.id) }}></TiTick>
+        <RiCloseCircleLine className='cross' onClick={() => { removeTask(todo.id) }}></RiCloseCircleLine>
+      </div>
     </div>
   ))
-  
 }
