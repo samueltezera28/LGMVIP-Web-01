f6ce2a9fe4cfeb946d8086cfc8e4619a57c1f09b
samueltezera28 <samueltezera28@gmail.com>
2023-08-08 23:30:01 +0300

    todolist

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,45 +1,45 @@
-import React from 'react'
+import React from 'react';
 import { useState } from 'react';
-
-import TodoForm from './TodoForms';
 import Todo from './Todo';
-
+import TodoForm from './TodoForm'
 
 export default function TodoList() {
 
-    //state Array which hold all todo
-    const [todos, setTodos] = useState([]);
 
-    const addTask = task => {
-        if(!task.text){
-            return
-        }
+  // State Array which hold all TODOS
+  const [todos, setTodos] = useState([]);
 
-        const newTodos = [task,...todos];
-        setTodos(newTodos);
+
+  // Add todo to list
+  const addTask = task => {
+    if (!task.text) {
+      return
     }
+    const newTodos = [task, ...todos];
+    setTodos(newTodos);
+  }
 
-    //remove todo from list
-    const removeTask = id =>{
-        let updatedTasks = [...todos].filter(task => task.id !== id)
-        setTodos(updatedTasks)
-    }
+  // Remove todo from list
+  const removeTask = id => {
+    let updatedTasks = [...todos].filter(task => task.id !== id)
+    setTodos(updatedTasks)
+  }
+
+  // Task is completed
+  const completeTask = id => {
+    let updatedTasks = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isComplete = true;
+      }
+      return todo;
+    })
+    setTodos(updatedTasks)
+
+  }
 
 
-    //task is completed
-    const completeTask = id => {
-        let updatedTasks = todos.map(todo => {
-            if(todo.id === id){
-                todo.isCompleted = true;
-            }
-            return todo;
-        })
-        setTodos(updatedTasks)
-    }
-  return (
-    <div>
-        <TodoForm addTask={addTask}></TodoForm>
-        <Todo todos={todos} completeTask ={completeTask} removeTask={removeTask}></Todo>
-    </div>
-  )
+  return <div>
+    <TodoForm addTask={addTask}></TodoForm>
+    <Todo todos={todos} completeTask={completeTask} removeTask={removeTask} ></Todo>
+  </div>;
 }
